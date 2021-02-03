94c227df8230eae33385da811aa68c2ed47e453a
jobayer-h <jobayer.dev@gmail.com>
2021-02-04 00:58:04 +0600

    send appoinment to MongoDB

--- server.js
+++ server.js
@@ -1,19 +1,41 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
+const MongoClient = require('mongodb').MongoClient;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASS}@cluster0.rwsfd.mongodb.net/doctorsportalDB?retryWrites=true&w=majority`;
+
 const app = express();
 
 const port = 4000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 
-app.get('/', (req, res) =>{
-    res.send('Hellw')
+
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true , useUnifiedTopology: true });
+client.connect(err => {
+    const collection = client.db("doctorsportalDB").collection("appoinment");
+    app.post('/addAppoinment', (req, res) => {
+        const appoinment = req.body;
+
+        collection.insertOne(appoinment)
+        .then((response) => {
+            if (response.insertedCount >= 1) {
+                res.send("Appoinment Added Successfully")
+            }
+        })
+    });
 });
 
-app.post('/addAppoinment', (req, res) =>{
-    console.log(req.body);
-    res.send('Appoinment Added Successfull')
-})
+
+app.get('/', (req, res) => {
+    res.send('Doctors Portal Backend')
+});
+
+
 
 app.listen(process.env.PORT || port);
